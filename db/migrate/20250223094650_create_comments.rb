cae78adef1a8b83b026b4ce84b3ac28044a201a5
Hiro-25 <63336620+Hiro-25@users.noreply.github.com>
2025-02-24 15:25:34 +0900

    comments_table_migration

--- db/migrate/20250223094650_create_comments.rb
+++ db/migrate/20250223094650_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :prototype_id
       t.text :content
       t.references :prototype, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
